8392b42f579a73a17c876d1d2bc9608d8ec068dd
Giovanni <1300687449@qq.com>
2019-04-01 21:11:41 +0800

    2019-4-1

---
+++ LexicalAnalysis_c/LexicalAnalysis_c/LexicalAnalysis_c.cpp
@@ -0,0 +1,267 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+
+using namespace std;
+
+#define NULL 0
+//基本数据类型
+#define INT 1
+#define FLOAT 2
+#define CHAR 3
+#define SHORT 4
+#define DOUBLE 5
+#define BOOL 6
+//系统保留字
+#define MAIN 7
+#define RETURN 8
+#define IF 9
+#define ELSE 10
+#define FOR 11
+#define WHILE 12
+#define VOID 13
+//运算符
+#define LT 14 //<
+#define LE 15 //<=
+#define GT 16 //>
+#define GE 17 //>=
+#define NE 18 //!=
+#define EQ 19 //==
+#define PLUSOP 20 //+
+#define MINUSOP 21 //-
+#define MULTIPLYOP 22 //*
+#define DIVIDIONOP 23 //'/'
+#define ANDOP 24 //&&
+#define OROP 25 //||
+#define BITANDOP 26 //&
+#define BITOROP 27 //|
+#define BITNOTOP 28 //!
+//分界符
+#define LBB 29 //{
+#define RBB 30 //}
+#define LSB 31 //(
+#define RSB 32 //)
+#define ASSIGN 33 //=
+#define SEMI 34 //;
+#define COMMA 35 //,
+//用户自定义标识符
+#define ID 36
+//常数标识符
+#define NUM 37
+//错误字符
+#define EROOR 38
+
+class LexicalAnalysis_c
+{
+private:
+    string token;
+	string preStr;
+    char ch;
+    vector<string> keyWords;
+	vector<string> dataTypes;
+	ifstream in;
+public:
+    LexicalAnalysis_c();
+	void readFile();
+	void getsym();
+	void getbc();
+    void strcatc();
+    bool isletter();
+    bool isdigit();
+    int reserve();
+    void retract();
+    void output(int category,void* point);
+	void scanner();
+	void error();
+};
+
+LexicalAnalysis_c::LexicalAnalysis_c(){
+	keyWords = { "main","return","if","else","for","while","void" };
+	dataTypes = { "int","float","char","short","double","bool" };
+}
+
+void LexicalAnalysis_c::readFile()
+{
+	/*string path;
+	cin >> path;*/
+	in.open("source.c");
+}
+
+void LexicalAnalysis_c::getsym()
+{
+	ch = in.get();
+}
+
+void LexicalAnalysis_c::getbc()
+{
+	while (ch == ' ' || ch == '\n') {
+		ch = in.get();
+	}
+}
+
+void LexicalAnalysis_c::strcatc()
+{
+	token += ch;
+}
+
+bool LexicalAnalysis_c::isletter()
+{
+	if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z')) return true;
+	return false;
+}
+
+bool LexicalAnalysis_c::isdigit()
+{
+	if (ch >= '0'&&ch <= '9') return true;
+	return false;
+}
+
+int LexicalAnalysis_c::reserve()
+{
+	for (int i = 0; i < keyWords.size(); i++) {
+		if (keyWords[i] == token) return 7 + i;
+	}
+	for (int i = 0; i < dataTypes.size(); i++) {
+		if (dataTypes[i] == token) return 1 + i;
+	}
+	return 0;
+}
+
+void LexicalAnalysis_c::retract()
+{
+	//token = token.substr(0, token.size() - 1);
+	in.seekg(-1, ios::cur);
+}
+
+void LexicalAnalysis_c::output(int flag, void * point)
+{
+	cout << "Flag:" << flag << "  Point:" << point << endl;
+}
+
+void LexicalAnalysis_c::scanner()
+{
+	token = "";
+	preStr = "";
+	readFile();
+	getsym();
+	getbc();
+	while (true) {
+		if (ch == EOF) break;
+		if (isletter() || (ch == '_')) {
+			strcatc();
+			getsym();
+			while (isletter() || isdigit() || (ch == '_')) {
+				strcatc();
+				getsym();
+			}
+			retract();
+			int flag = reserve();
+			if (!flag) {
+				output(ID, NULL);
+			}
+			else {
+				output(flag, NULL);
+			}
+		}
+		else if (isdigit()) {
+			strcatc();
+			getsym();
+			while (isdigit()) {
+				strcatc();
+				getsym();
+			}
+			retract();
+			output(NUM, NULL);
+		}
+		else if (ch == '+') output(PLUSOP, NULL);
+		else if (ch == '-') output(DIVIDIONOP, NULL);
+		else if (ch == '*') output(MULTIPLYOP, NULL);
+		else if (ch == '/') output(DIVIDIONOP, NULL);
+		else if (ch == '|') {
+			strcatc();
+			getsym();
+			if (ch == '|')
+				output(OROP, NULL);
+			else {
+				retract();
+				output(BITOROP, NULL);
+			}
+		}
+		else if (ch == '&') {
+			strcatc();
+			getsym();
+			if (ch == '&') output(ANDOP, NULL);
+			else {
+				retract();
+				output(BITANDOP, NULL);
+			}
+		}
+		else if (ch == '>') {
+			strcatc();
+			getsym();
+			if (ch == '=')	output(GE, NULL);
+			else
+			{
+				retract();
+				output(GT, NULL);
+			}
+		}
+		else if (ch == '<') {
+			strcatc();
+			getsym();
+			if (ch == '=')	output(LE, NULL);
+			else
+			{
+				retract();
+				output(LT, NULL);
+			}
+		}
+		
+		else if (ch == '{') output(LBB, NULL);
+		else if (ch == '}') output(RBB, NULL);
+		else if (ch == '(') output(LSB, NULL);
+		else if (ch == ')') output(RSB, NULL);
+		else if (ch == '=')
+		{
+			strcatc();
+			getsym();
+			if (ch == '=')	output(EQ, NULL);
+			else
+			{
+				retract();
+				output(ASSIGN, NULL);
+			}
+		}
+		else if (ch == '!')
+		{
+			strcatc();
+			getsym();
+			if (ch == '=')	output(NE, NULL);
+			else
+			{
+				retract();
+				output(BITNOTOP, NULL);
+			}
+		}
+		else if (ch == ';') output(SEMI, NULL);
+		else if (ch == ',') output(COMMA, NULL);
+		else error();
+		token = "";
+		getsym();
+		getbc();
+	}
+}
+
+void LexicalAnalysis_c::error()
+{
+	cout << "ERROR" << endl;
+}
+
+
+int main() {
+	LexicalAnalysis_c analysis;
+	analysis.scanner();
+	system("pause");
+	return 0;
+}
---
+++ LexicalAnalysis_c/LexicalAnalysis_c/source.c
@@ -0,0 +1,16 @@
+int a,b,c;
+int main()
+{
+    char ss1,ss2,ss3;
+    int x1,1y,_x1,100;
+    double x1,x2;
+    1y=x1*x1+0.3;
+    x1!=y1+1e2;
+    y1=x1&&x2;
+    y1>=x2;
+    if(x1>=1)
+    {
+        return 1;
+    }
+    return 0;
+}
