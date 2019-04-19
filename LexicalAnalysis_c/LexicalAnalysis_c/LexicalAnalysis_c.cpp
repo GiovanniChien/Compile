999a5e56f89a2bb9489bee85bc901b6933108251
Giovanni <1300687449@qq.com>
2019-04-19 20:03:41 +0800

    2019-4-19

--- LexicalAnalysis_c/LexicalAnalysis_c/LexicalAnalysis_c.cpp
+++ LexicalAnalysis_c/LexicalAnalysis_c/LexicalAnalysis_c.cpp
@@ -1,11 +1,10 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
-#define NULL 0
 //基本数据类型
 #define INT 1
 #define FLOAT 2
@@ -22,29 +21,29 @@ using namespace std;
 #define WHILE 12
 #define VOID 13
 //运算符
-#define LT 14 //<
-#define LE 15 //<=
-#define GT 16 //>
-#define GE 17 //>=
-#define NE 18 //!=
-#define EQ 19 //==
-#define PLUSOP 20 //+
-#define MINUSOP 21 //-
+#define LT 14		  //<
+#define LE 15		  //<=
+#define GT 16		  //>
+#define GE 17		  //>=
+#define NE 18		  //!=
+#define EQ 19		  //==
+#define PLUSOP 20	 //+
+#define MINUSOP 21	//-
 #define MULTIPLYOP 22 //*
 #define DIVIDIONOP 23 //'/'
-#define ANDOP 24 //&&
-#define OROP 25 //||
-#define BITANDOP 26 //&
-#define BITOROP 27 //|
-#define BITNOTOP 28 //!
+#define ANDOP 24	  //&&
+#define OROP 25		  //||
+#define BITANDOP 26   //&
+#define BITOROP 27	//|
+#define BITNOTOP 28   //!
 //分界符
-#define LBB 29 //{
-#define RBB 30 //}
-#define LSB 31 //(
-#define RSB 32 //)
+#define LBB 29	//{
+#define RBB 30	//}
+#define LSB 31	//(
+#define RSB 32	//)
 #define ASSIGN 33 //=
-#define SEMI 34 //;
-#define COMMA 35 //,
+#define SEMI 34   //;
+#define COMMA 35  //,
 //用户自定义标识符
 #define ID 36
 //常数标识符
@@ -52,33 +51,55 @@ using namespace std;
 //错误字符
 #define EROOR 38
 
+//标识符表
+struct Indentifier {
+	string name;//标识符
+	int type;//数据类型
+	Indentifier* parent;
+	vector<Indentifier>* child;
+};
+
 class LexicalAnalysis_c
 {
-private:
-    string token;
+  private:
+	string token;
 	string preStr;
-    char ch;
-    vector<string> keyWords;
+	int preType;
+	Indentifier* curParent;
+	char ch;
+	vector<string> keyWords;
 	vector<string> dataTypes;
 	ifstream in;
-public:
-    LexicalAnalysis_c();
+	vector<void *> constValueList; //常量表
+	vector<void *> signalList;	 //标识符表
+	vector<Indentifier> indentifiers;//标识符邻接表
+	vector<void *> keyList;		   //保留字表
+  public:
+	LexicalAnalysis_c();
 	void readFile();
 	void getsym();
 	void getbc();
-    void strcatc();
-    bool isletter();
-    bool isdigit();
-    int reserve();
-    void retract();
-    void output(int category,void* point);
+	void strcatc();
+	bool isletter();
+	bool isdigit();
+	int reserve();
+	void retract();
+	void output(int category, void *point);
+	void output(string token, int category, void *point);
 	void scanner();
 	void error();
+	void *buildList(int i); //i=0表示构建一个数字常量表 i=1表示构建一个符号表 i=2表示构建一个保留字表
+	int findInditifier(const string name);
+	Indentifier* buildInditifier(const string name, const int type);
 };
 
-LexicalAnalysis_c::LexicalAnalysis_c(){
-	keyWords = { "main","return","if","else","for","while","void" };
-	dataTypes = { "int","float","char","short","double","bool" };
+LexicalAnalysis_c::LexicalAnalysis_c()
+{
+	preStr = "";
+	preType = 0;
+	curParent = NULL;
+	keyWords = {"main", "return", "if", "else", "for", "while", "void"};
+	dataTypes = {"int", "float", "char", "short", "double", "bool"};
 }
 
 void LexicalAnalysis_c::readFile()
@@ -95,7 +116,8 @@ void LexicalAnalysis_c::getsym()
 
 void LexicalAnalysis_c::getbc()
 {
-	while (ch == ' ' || ch == '\n') {
+	while (ch == ' ' || ch == '\n')
+	{
 		ch = in.get();
 	}
 }
@@ -107,23 +129,29 @@ void LexicalAnalysis_c::strcatc()
 
 bool LexicalAnalysis_c::isletter()
 {
-	if ((ch >= 'a'&&ch <= 'z') || (ch >= 'A'&&ch <= 'Z')) return true;
+	if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+		return true;
 	return false;
 }
 
 bool LexicalAnalysis_c::isdigit()
 {
-	if (ch >= '0'&&ch <= '9') return true;
+	if (ch >= '0' && ch <= '9')
+		return true;
 	return false;
 }
 
 int LexicalAnalysis_c::reserve()
 {
-	for (int i = 0; i < keyWords.size(); i++) {
-		if (keyWords[i] == token) return 7 + i;
+	for (int i = 0; i < keyWords.size(); i++)
+	{
+		if (keyWords[i] == token)
+			return 7 + i;
 	}
-	for (int i = 0; i < dataTypes.size(); i++) {
-		if (dataTypes[i] == token) return 1 + i;
+	for (int i = 0; i < dataTypes.size(); i++)
+	{
+		if (dataTypes[i] == token)
+			return 1 + i;
 	}
 	return 0;
 }
@@ -134,9 +162,61 @@ void LexicalAnalysis_c::retract()
 	in.seekg(-1, ios::cur);
 }
 
-void LexicalAnalysis_c::output(int flag, void * point)
+void LexicalAnalysis_c::output(int flag, void *point)
 {
 	cout << "Flag:" << flag << "  Point:" << point << endl;
+}
+
+void LexicalAnalysis_c::output(string token, int flag, void *point)
+{
+	cout << "Value:" << token << " Flag:" << flag << " Point:" << point << endl;
+}
+
+void *LexicalAnalysis_c::buildList(int i)
+{
+	void *p = new string(token);
+	switch (i)
+	{
+	case 0: //常量表
+		constValueList.push_back(p);
+		break;
+	case 1: //标识符表
+		signalList.push_back(p);
+		break;
+	case 2: //保留字表
+		signalList.push_back(p);
+		break;
+	}
+	return p;
+}
+
+int LexicalAnalysis_c::findInditifier(const string name)
+{
+	if (curParent->child == NULL) return -1;
+	vector<Indentifier> vectors = *curParent->child;
+	for (int i = 0; i < vectors.size(); i++) {
+		Indentifier tmp = vectors[i];
+		if (tmp.name == name) return i;		//找到同名变量,重定义
+	}
+	return -1;
+}
+
+Indentifier* LexicalAnalysis_c::buildInditifier(const string name, const int type)
+{
+	Indentifier indentifier;
+	indentifier.parent = curParent;
+	indentifier.child = NULL;
+	indentifier.type = type;
+	if (curParent->child == NULL) {
+		vector<Indentifier> child;
+		child.push_back(indentifier);
+		curParent->child = &child;
+	}
+	else {
+		vector<Indentifier> child = *curParent->child;
+		child.push_back(indentifier);
+	}
+	return &indentifier;
 }
 
 void LexicalAnalysis_c::scanner()
@@ -146,87 +226,128 @@ void LexicalAnalysis_c::scanner()
 	readFile();
 	getsym();
 	getbc();
-	while (true) {
-		if (ch == EOF) break;
-		if (isletter() || (ch == '_')) {
+	while (true)
+	{
+		if (ch == EOF)
+			break;
+		if (isletter() || (ch == '_'))
+		{
 			strcatc();
 			getsym();
-			while (isletter() || isdigit() || (ch == '_')) {
+			while (isletter() || isdigit() || (ch == '_'))
+			{
 				strcatc();
 				getsym();
 			}
 			retract();
 			int flag = reserve();
-			if (!flag) {
-				output(ID, NULL);
+			if (!flag)
+			{
+				//标识符
+				int res = findInditifier(token);
+				if (res != -1) {
+					cout << "变量 " << token << " 重定义" << endl;
+				}
+				else if (preType == 0) {
+					cout << "变量 " << token << "定义非法" << endl;
+				}
+				else {
+					output(token, ID, buildInditifier(token, preType));
+				}
 			}
-			else {
-				output(flag, NULL);
+			else
+			{
+				if (flag >= 1 && flag <= 6)
+					preType = flag;
+				output(token, flag, buildList(2));
 			}
+			preStr = token;
 		}
-		else if (isdigit()) {
+		else if (isdigit())
+		{
 			strcatc();
 			getsym();
-			while (isdigit()) {
+			while (isdigit())
+			{
 				strcatc();
 				getsym();
 			}
 			retract();
-			output(NUM, NULL);
+			output(token, NUM, buildList(0));
+			preType = 0;
+			preStr = "";
+			//token="";
 		}
-		else if (ch == '+') output(PLUSOP, NULL);
-		else if (ch == '-') output(DIVIDIONOP, NULL);
-		else if (ch == '*') output(MULTIPLYOP, NULL);
-		else if (ch == '/') output(DIVIDIONOP, NULL);
-		else if (ch == '|') {
+		else if (ch == '+')
+			output(PLUSOP, NULL);
+		else if (ch == '-')
+			output(DIVIDIONOP, NULL);
+		else if (ch == '*')
+			output(MULTIPLYOP, NULL);
+		else if (ch == '/')
+			output(DIVIDIONOP, NULL);
+		else if (ch == '|')
+		{
 			strcatc();
 			getsym();
 			if (ch == '|')
 				output(OROP, NULL);
-			else {
+			else
+			{
 				retract();
 				output(BITOROP, NULL);
 			}
 		}
-		else if (ch == '&') {
+		else if (ch == '&')
+		{
 			strcatc();
 			getsym();
-			if (ch == '&') output(ANDOP, NULL);
-			else {
+			if (ch == '&')
+				output(ANDOP, NULL);
+			else
+			{
 				retract();
 				output(BITANDOP, NULL);
 			}
 		}
-		else if (ch == '>') {
+		else if (ch == '>')
+		{
 			strcatc();
 			getsym();
-			if (ch == '=')	output(GE, NULL);
+			if (ch == '=')
+				output(GE, NULL);
 			else
 			{
 				retract();
 				output(GT, NULL);
 			}
 		}
-		else if (ch == '<') {
+		else if (ch == '<')
+		{
 			strcatc();
 			getsym();
-			if (ch == '=')	output(LE, NULL);
+			if (ch == '=')
+				output(LE, NULL);
 			else
 			{
 				retract();
 				output(LT, NULL);
 			}
 		}
-		
-		else if (ch == '{') output(LBB, NULL);
-		else if (ch == '}') output(RBB, NULL);
-		else if (ch == '(') output(LSB, NULL);
-		else if (ch == ')') output(RSB, NULL);
+		else if (ch == '{')
+			output(LBB, NULL);
+		else if (ch == '}')
+			output(RBB, NULL);
+		else if (ch == '(')
+			output(LSB, NULL);
+		else if (ch == ')')
+			output(RSB, NULL);
 		else if (ch == '=')
 		{
 			strcatc();
 			getsym();
-			if (ch == '=')	output(EQ, NULL);
+			if (ch == '=')
+				output(EQ, NULL);
 			else
 			{
 				retract();
@@ -237,16 +358,20 @@ void LexicalAnalysis_c::scanner()
 		{
 			strcatc();
 			getsym();
-			if (ch == '=')	output(NE, NULL);
+			if (ch == '=')
+				output(NE, NULL);
 			else
 			{
 				retract();
 				output(BITNOTOP, NULL);
 			}
 		}
-		else if (ch == ';') output(SEMI, NULL);
-		else if (ch == ',') output(COMMA, NULL);
-		else error();
+		else if (ch == ';')
+			output(SEMI, NULL);
+		else if (ch == ',')
+			output(COMMA, NULL);
+		else
+			error();
 		token = "";
 		getsym();
 		getbc();
@@ -258,8 +383,8 @@ void LexicalAnalysis_c::error()
 	cout << "ERROR" << endl;
 }
 
-
-int main() {
+int main()
+{
 	LexicalAnalysis_c analysis;
 	analysis.scanner();
 	system("pause");
